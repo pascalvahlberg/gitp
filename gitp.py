d156ed16e65ca486f5a652354391e11b1cfa6232
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-04-15 19:46:34 +0200

    [2] Other/Checksum: c81e728d9d4c2f636f067f89cc14862c
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- gitp.py
+++ gitp.py
@@ -8,23 +8,29 @@ try:
 	i = 1000
 	revision = Popen("git log --oneline | wc -l", shell=True, stdout=PIPE).stdout.read().rstrip()
 	revision = str(int(revision) + 1)
-	if len(str(revision)) > 3:
-		version = revision[:-3]+"."+revision[-3:]
-	elif len(str(revision)) == 3:
-		version = "0."+revision
-	elif len(str(revision)) == 2:
-		version = "0.0"+revision
+
+	if len(revision) > 3:
+		version = revision[:-3] + "." + revision[-3:]
+	elif len(revision) == 3:
+		version = "0." + revision
+	elif len(revision) == 2:
+		version = "0.0" + revision
 	else:
-		version = "0.00"+revision
+		version = "0.00" + revision
+
 	f = open("version", "w")
 	f.write(version)
 	f.close()
 	Popen("git add .", shell=True).wait()
+
 	if len(argv) > 1:
 		commit = "["+revision+"] "+' '.join(argv[1:])
 	else:
-		commit = "["+revision+"] checksum: "+md5(revision).hexdigest()
-	Popen("git commit -m '"+commit+"' -s", shell=True).wait()
+		commit = "["+revision+"] Other/Checksum: "+md5(revision).hexdigest()
+
+	Popen("git commit -m '" + commit + "' -s", shell=True).wait()
 	Popen("git push origin master", shell=True).wait()
-except Exception,e: print(e)
+except Exception,e:
+	print(e)
+
 exit(0)
