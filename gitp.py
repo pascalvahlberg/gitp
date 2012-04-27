d4ea975e26a6fd83e50df0cbb7ac9766f11f5ff2
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-04-27 13:44:17 +0200

    [18] Other/Checksum: 9089324eb9d654b0debfda1b36774589
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- gitp.py
+++ gitp.py
@@ -29,10 +29,10 @@ try:
 		file_writer.close()
 
 	for lists in open("list", "r"):
-		filename = lists.rstrip().split()[0]
-		list_checksum = lists.rstrip().split()[1]
+		filename = lists.rstrip()[:-33]
+		list_checksum = lists.rstrip().split()[-32:]
 		if not access(filename, R_OK):
-			Popen("git rm " + filename.replace(" ", "\\ "), shell=True).wait()
+			Popen("git rm " + filename.replace(" ", "\ "), shell=True).wait()
 
 	file_writer = open("list", "w")
 	file_writer.write("")
