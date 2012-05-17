245acb1e5fffb6d9bcb750f9a46a051382c1d148
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-05-17 17:04:09 +0200

    [36] Other/Checksum: 8201ec4c46ce2669b196a8fab687a9eb
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- gitp.py
+++ gitp.py
@@ -7,19 +7,28 @@ from fnmatch import fnmatch
 from os import access, walk, path, R_OK
 
 try:
+	def red(string):
+		return("\033[91m" + string + "\033[0m")
+
+	def blue(string):
+		return("\033[94m" + string + "\033[0m")
+
+	def green(string):
+		return("\033[92m" + string + "\033[0m")
+
 	refs = Popen("git remote", shell=True, stdout=PIPE).stdout.read().rstrip()
 
 	for ref in refs.splitlines():
-		print("% Fetching '" + ref + "'")
+		print(green("*") + " Fetching '" + ref + "'")
 		Popen("git pull --quiet " + ref + " master", shell=True, stdout=PIPE).stdout.read().rstrip()
 
 	if not access("list", R_OK):
-		print("% Creating list")
+		print(blue("*") + " Creating list")
 		file_writer = open("list", "w")
 		file_writer.write("")
 		file_writer.close()
 
-	print("% Creating listprehash")
+	print(blue("*") + " Creating listprehash")
 	listfile = open("list", "r")
 	listprehash = md5(listfile.read()).hexdigest()
 	listfile.close()
@@ -28,10 +37,10 @@ try:
 		filename = lists.rstrip()[:-33]
 		list_checksum = lists.rstrip().split()[-32:]
 		if not access(filename, R_OK):
-			print("% Removing '" + filename + "'")
+			print(blue("*") + " Removing '" + filename + "'")
 			Popen("git rm --cached " + filename.replace(" ", "\ "), shell=True, stdout=PIPE).stdout.read().rstrip()
 
-	print("% Clearing list")
+	print(blue("*") + " Clearing list")
 	file_writer = open("list", "w")
 	file_writer.write("")
 
@@ -51,23 +60,23 @@ try:
 					file_content = file_reader.read()
 					file_reader.close()
 					file_checksum = md5(file_content).hexdigest()
-					print("% Adding '" + path.join(root[2:], name) + "'")
+					print(blue("*") + " Adding '" + path.join(root[2:], name) + "'")
 					file_writer.write(path.join(root[2:], name) + " " + file_checksum + "\n")
 
 	file_writer.close()
 
-	print("% Creating listhash")
+	print(blue("*") + " Creating listhash")
 	listfile = open("list", "r")
 	listhash = md5(listfile.read()).hexdigest()
 	listfile.close()
 
-	print("% Comparing listhashes")
+	print(blue("*") + " Comparing listhashes")
 	if listprehash != listhash:
 		revision = Popen("git log --oneline | wc -l", shell=True, stdout=PIPE).stdout.read().rstrip()
-		print("% Getting new revision")
+		print(blue("*") + " Getting new revision")
 		revision = str(int(revision) + 1)
 
-		print("% Creating version")
+		print(blue("*") + " Creating version")
 		if len(revision) > 3:
 			version = revision[:-3] + "." + revision[-3:]
 		elif len(revision) == 3:
@@ -77,7 +86,7 @@ try:
 		else:
 			version = "0.00" + revision
 
-		print("% Writing version")
+		print(blue("*") + " Writing version")
 		f = open("version", "w")
 		f.write(version)
 		f.close()
@@ -93,18 +102,18 @@ try:
 			file_checksum = md5(file_content).hexdigest()
 			commit = "Other/Checksum: " + file_checksum
 
-		print("% Committing '" + commit + "'")
+		print(blue("*") + " Committing '" + commit + "'")
 		Popen("git commit -m '[" + revision + "] " + commit + "' -s", shell=True, stdout=PIPE).stdout.read().rstrip()
-		print("% Pushing to repository")
+		print(green("*") + " Pushing to repository")
 		Popen("git push origin master", shell=True).wait()
 	else:
-		print("% No update needed.")
+		print(green("*") + " No update needed.")
 
 except Exception,e:
-	print(e)
+	print(red("*") + " " + e)
 	exit(0)
 except KeyboardInterrupt,e:
-	print("Ctrl + C")
+	print(red("*") + " Ctrl + C")
 	exit(0)
 
 exit(0)
